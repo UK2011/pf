3de6e57a6dfb9e37596517d2547170b1233f8e2a
uk2011 <saharshkhurana@gmail.com>
2026-08-16 17:15:06 +0530

    Navbar setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="menu">
+
+            <div className='menu-bg'>
+
+            </div>
+
+
+            <a className="menu-item" href="/projects">
+                <span className="item-index">01</span>
+                <span className="item-label">Projects</span>
+                <span className="item-end"></span>
+            </a>
+            <a className="menu-item sans" href="/about">
+                <span className="item-index">02</span>
+                <span className="item-label">About</span>
+                <span className="item-end"></span>
+            </a>
+            <a className="menu-item" href="/contact">
+                <span className="item-index">03</span>
+                <span className="item-label">Contact</span>
+                <span className="item-end"></span>
+            </a>
+
+
+        </nav>
     )
 }
 
